a9288fea73ad47dc9cbad9045862da67fb84dc7c
arv.github.io <arvenabrigo91@gmail.com>
2026-09-17 19:01:50 +0800

    update database connection

--- db.php
+++ db.php
@@ -1,11 +1,19 @@
+
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "studentt_dbb";
+$servername = getenv("DB_HOST");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$dbname = getenv("DB_NAME");
+$port = getenv("DB_PORT") ?: 3306;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli(
+    $servername,
+    $username,
+    $password,
+    $dbname,
+    $port
+);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
